43b4b0483b80fd2726756c58bdd7b9bf948b0c48
Alex Krantz <alex@krantz.dev>
2022-09-27 20:03:39 -0700

    Remove unnecessary serialization of props

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -6,17 +6,17 @@ import { getAllPostMetadata } from '@/lib/blog';
 import type { FrontMatter } from '@/types';
 
 interface Props {
-  metadata: string;
+  metadata: FrontMatter[];
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const metadata = await getAllPostMetadata();
-  return { props: { metadata: JSON.stringify(metadata) } };
+  return { props: { metadata } };
 };
 
-const Blog = ({ metadata: rawMeta }: Props): JSX.Element => {
-  const metadata = JSON.parse(rawMeta) as FrontMatter[];
-  const latest = metadata.shift() as FrontMatter;
+const Blog = ({ metadata }: Props): JSX.Element => {
+  const latest = metadata[0];
+  const rest = metadata.slice(1);
 
   return (
     <Layout seo={{ title: 'Alex Krantz - Blog' }}>
@@ -24,7 +24,7 @@ const Blog = ({ metadata: rawMeta }: Props): JSX.Element => {
         <div className="relative max-w-6xl mx-auto">
           <BlogPostCard latest {...latest} />
           <div className="mt-4 lg:mt-12 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:max-w-none">
-            {metadata.map((post, i) => (
+            {rest.map((post, i) => (
               <BlogPostCard key={i} {...post} />
             ))}
           </div>
