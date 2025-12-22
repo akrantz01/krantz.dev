511fe706cd40d1950fdaff188ae7b6dc10a76ef0
Alex Krantz <alex@krantz.dev>
2025-12-22 00:00:49 -0800

    Wipe next.js version

--- src/types/props.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentProps, JSXElementConstructor } from 'react';
-
-/**
- * Extracts the prop types from the typeof of a component
- */
-export type WithProps<T extends keyof JSX.IntrinsicElements | JSXElementConstructor<U>, U = any> = ComponentProps<T>;
