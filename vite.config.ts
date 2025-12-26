257e2d126871e0b2db63cf3b7edb25b2d1c71cdd
Alex Krantz <alex@krantz.dev>
2025-12-26 13:54:27 -0800

    Add chrome devtools integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 import generateFavicons from './plugins/generate-favicons';
 
 export default defineConfig({
-	plugins: [generateFavicons({ source: 'src/lib/assets/favicon.svg' }), sveltekit()]
+	plugins: [devtoolsJson(), generateFavicons({ source: 'src/lib/assets/favicon.svg' }), sveltekit()]
 });
