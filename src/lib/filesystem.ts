0676aa61c1f03337cfda8487ae9f0de244b3da8b
Alex Krantz <alex@krantz.dev>
2025-12-25 23:54:08 -0800

    Add markdown-aware filesystem

--- src/lib/filesystem.ts
+++ src/lib/filesystem.ts
@@ -1,7 +1,9 @@
-export default class FileSystem {
-	private content: Map<string, () => Promise<string>>;
+import matter from 'gray-matter';
 
-	constructor(modules: Record<string, () => Promise<string>>) {
+export class FileSystem<C> {
+	protected content: Map<string, C>;
+
+	constructor(modules: Record<string, C>) {
 		const prefix = findDirectoryPrefix(Object.keys(modules));
 		this.content = new Map(
 			Object.entries(modules).map(([k, v]) => [k.substring(prefix.length), v])
@@ -12,14 +14,47 @@ export default class FileSystem {
 		return this.content.keys().toArray();
 	}
 
-	async read(name: string): Promise<string | null> {
+	read(name: string): C | null {
 		const reader = this.content.get(name);
 		if (reader === undefined) return null;
-		return reader();
+		return reader;
 	}
 
 	exists(name: string): boolean {
 		return this.content.has(name);
+	}
+}
+
+export interface MarkdownFile<M extends { [key: string]: unknown }> {
+	path: string;
+	meta: M | null;
+}
+
+export class MarkdownFileSystem<M extends { [key: string]: unknown }> extends FileSystem<string> {
+	private metadata: Map<string, M | null>;
+
+	constructor(modules: Record<string, string>) {
+		super(modules);
+
+		this.metadata = new Map();
+		this.content.forEach((value, path) => {
+			const fm = matter(value, { language: 'yaml' });
+			this.content.set(path, fm.content);
+			this.metadata.set(path, fm.data);
+		});
+	}
+
+	listWithMeta(): MarkdownFile<M>[] {
+		return this.metadata
+			.entries()
+			.map(([path, meta]) => ({ path, meta }))
+			.toArray();
+	}
+
+	meta(name: string): M | null {
+		const meta = this.metadata.get(name);
+		if (meta === undefined) return null;
+		return meta;
 	}
 }
 
