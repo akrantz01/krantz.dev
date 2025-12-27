f5acbe0717d3df2f15ae88d8d2ee5278e7b59f43
Alex Krantz <alex@krantz.dev>
2025-12-26 17:29:01 -0800

    Order posts by date descending

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,4 +1,9 @@
 import type { PageServerLoad } from './$types';
 import { fs } from '$lib/server/posts';
+import { compareDesc } from 'date-fns';
 
-export const load: PageServerLoad = () => ({ posts: fs.listWithFrontmatter() });
+export const load: PageServerLoad = () => {
+	const posts = fs.listWithFrontmatter();
+	posts.sort((a, b) => compareDesc(a.meta.date, b.meta.date));
+	return { posts };
+};
