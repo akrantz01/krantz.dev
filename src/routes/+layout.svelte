f178c06b272fc20bd9445cd45e624f03458d0651
Alex Krantz <alex@krantz.dev>
2025-12-28 00:22:59 -0800

    Add global class for screenreader only text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -266,5 +266,17 @@
 			margin: 0 auto;
 			padding: 1rem 0;
 		}
+
+		.sr-only {
+			position: absolute;
+			width: 1px;
+			height: 1px;
+			padding: 0;
+			margin: -1px;
+			overflow: hidden;
+			clip-path: inset(50%);
+			white-space: nowrap;
+			border-width: 0;
+		}
 	}
 </style>
