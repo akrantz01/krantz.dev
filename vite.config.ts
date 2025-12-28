aa58828b6e034d3d24e05c0ef258945b5a74596e
Alex Krantz <alex@krantz.dev>
2025-12-27 23:02:35 -0800

    Allow specifying vite allowed hosts via envvar

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
+import 'dotenv/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 
 import generateFavicons from './plugins/generate-favicons';
 
+const allowedHosts = (process.env.VITE_ALLOWED_HOSTS ?? '')
+	.split(',')
+	.map((domain) => domain.trim());
+
 export default defineConfig({
-	plugins: [devtoolsJson(), generateFavicons({ source: 'src/lib/assets/favicon.svg' }), sveltekit()]
+	plugins: [
+		devtoolsJson(),
+		generateFavicons({ source: 'src/lib/assets/favicon.svg' }),
+		sveltekit()
+	],
+	server: { allowedHosts },
+	preview: { allowedHosts }
 });
