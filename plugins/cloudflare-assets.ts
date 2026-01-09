7df2fb813b0fd6ebd8e12fdde3988df87f50b1a4
Alex Krantz <alex@krantz.dev>
2026-01-08 18:41:24 -0800

    Make cloudflare wasm plugin generic over various asset types

---
+++ plugins/cloudflare-assets.ts
@@ -0,0 +1,142 @@
+import type { Plugin } from 'vite';
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
+const MODULE_PREFIX = '\0cf-asset:';
+const MODULE_REGEXP = /^\0cf-asset:([a-z]+):(.+)$/g;
+
+interface GenBuild {
+	workerd: (exportName: string, ref: string) => string[];
+	nodejs: (exportName: string, ref: string) => string[];
+}
+
+interface Asset {
+	type: string;
+	extensions: string[];
+	genBuild: GenBuild;
+	genServe: (exportName: string, path: string) => string[];
+}
+
+export default function cloudflareAssetsPlugin(assets: Asset[]): Plugin {
+	const typeToAsset = new Map<string, Asset>();
+	for (const asset of assets) {
+		if (typeToAsset.has(asset.type))
+			throw new Error(`asset type '${asset.type}' is already defined`);
+
+		typeToAsset.set(asset.type, asset);
+	}
+
+	const additionalModulePaths = new Set<string>();
+	let isBuild: boolean;
+
+	return {
+		name: 'cloudflare-assets',
+		enforce: 'pre',
+		sharedDuringBuild: true,
+
+		configResolved({ command }) {
+			isBuild = command === 'build';
+		},
+
+		async resolveId(source, importer, options) {
+			// Ignore virtual modules and non-asset imports
+			if (source.startsWith('\0') || source.startsWith('/@id/__x00__')) return null;
+
+			const asset = assets.find((asset) => asset.extensions.some((ext) => source.endsWith(ext)));
+			if (asset === undefined) return null;
+
+			this.debug(`resolving ${asset.type} asset ${source}`);
+
+			const resolved = await this.resolve(cleanUrl(source), importer, {
+				...options,
+				skipSelf: true
+			});
+			if (!resolved) throw new Error(`Import "${source}" not found. Does the file exist?`);
+
+			this.debug(`resolved path: ${resolved.id}`);
+
+			additionalModulePaths.add(resolved.id);
+
+			if (resolved.id.startsWith(MODULE_PREFIX)) {
+				this.debug(`forwarding existing virtual module`);
+				return { id: resolved.id };
+			} else {
+				const id = `${MODULE_PREFIX}${asset.type}:${resolved.id}`;
+				this.debug(`registering virtual module: ${id}`);
+				return { id };
+			}
+		},
+
+		async load(id) {
+			const asset = extractAsset(id);
+			if (asset === null) return null;
+			this.debug(`loading ${asset.type} asset: ${asset.path}`);
+
+			const assetDef = typeToAsset.get(asset.type);
+			if (assetDef === undefined) {
+				this.warn(`asset definition '${asset.type}' not found for ${id}`);
+				return null;
+			}
+
+			const exportName = 'cfAsset_' + randomHex(6);
+
+			const code = [`let ${exportName};`];
+			if (isBuild) {
+				const fileName = path.basename(asset.path);
+				const source = await fs.readFile(asset.path);
+
+				const ref = this.emitFile({
+					type: 'asset',
+					name: fileName,
+					originalFileName: asset.path,
+					needsCodeReference: true,
+					source
+				});
+
+				code.push(
+					`if (typeof navigator !== 'undefined' && navigator.userAgent === 'Cloudflare-Workers') {`
+				);
+				code.push(...assetDef.genBuild.workerd(exportName, ref));
+				code.push(`} else {`);
+				code.push(...assetDef.genBuild.nodejs(exportName, ref));
+				code.push(`}`);
+			} else {
+				code.push(...assetDef.genServe(exportName, asset.path));
+			}
+
+			code.push(`export default ${exportName};`);
+			return { code: code.join('\n'), map: null };
+		},
+
+		hotUpdate({ file, server }) {
+			if (additionalModulePaths.has(file)) {
+				this.info(`asset changed: ${file}`);
+				server.restart();
+				return [];
+			}
+		}
+	};
+}
+
+const extractAsset = (id: string) => {
+	const matches = id.matchAll(MODULE_REGEXP);
+	if (matches === null) return null;
+
+	const { value } = matches.next();
+	if (value === undefined) return null;
+
+	const [, type, path] = value;
+	return { type, path };
+};
+
+const postfixRE = /[?#].*$/;
+const cleanUrl = (url: string): string => url.replace(postfixRE, '');
+
+const hexCharset = '1234567890ABCDEF';
+const randomHex = (length: number): string => {
+	let result = '';
+	for (let i = 0; i < length; i++) {
+		result += hexCharset.charAt(Math.floor(Math.random() * hexCharset.length));
+	}
+	return result;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,51 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 
-import cloudflareWasm from './plugins/cloudflare-wasm';
+import cloudflareAssetsPlugin from './plugins/cloudflare-assets';
 import generateFavicons from './plugins/generate-favicons';
+
+const cloudflareAssets = cloudflareAssetsPlugin([
+	{
+		type: 'wasm',
+		extensions: ['.wasm', '.wasm?module'],
+		genBuild: {
+			workerd: (exportName, ref) => [
+				`const module = await import(/* @vite-ignore */ import.meta.ROLLUP_FILE_URL_${ref});`,
+				`${exportName} = module.default;`
+			],
+			nodejs: (exportName, ref) => [
+				`const { readFile } = await import('node:fs/promises');`,
+				`const url = new URL(import.meta.ROLLUP_FILE_URL_${ref}, import.meta.url);`,
+				`const bytes = await readFile(url);`,
+				`${exportName} = await WebAssembly.compile(bytes);`
+			]
+		},
+		genServe: (exportName, path) => [
+			`import { readFile } from 'node:fs/promises';`,
+			`const bytes = await readFile(${JSON.stringify(path)});`,
+			`${exportName} = await WebAssembly.compile(bytes);`
+		]
+	},
+	{
+		type: 'font',
+		extensions: ['.woff', '.woff2'],
+		genBuild: {
+			workerd: (exportName, ref) => [
+				`const module = await import(/* @vite-ignore */ import.meta.ROLLUP_FILE_URL_${ref});`,
+				`${exportName} = module.default;`
+			],
+			nodejs: (exportName, ref) => [
+				`const { readFile } = await import('node:fs/promises');`,
+				`const url = new URL(import.meta.ROLLUP_FILE_URL_${ref}, import.meta.url);`,
+				`${exportName} = await readFile(url);`
+			]
+		},
+		genServe: (exportName, path) => [
+			`import { readFile } from 'node:fs/promises';`,
+			`${exportName} = await readFile(${JSON.stringify(path)});`
+		]
+	}
+]);
 
 const allowedHosts = (process.env.VITE_ALLOWED_HOSTS ?? '')
 	.split(',')
@@ -14,7 +57,7 @@ export default defineConfig({
 	plugins: [
 		devtoolsJson(),
 		generateFavicons({ source: 'src/lib/assets/favicon.svg' }),
-		cloudflareWasm(),
+		cloudflareAssets,
 		sveltekit()
 	],
 	server: {
