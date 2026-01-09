f6b200d3689bef5379326336b5e73d2724dfd2e7
Alex Krantz <alex@krantz.dev>
2026-01-08 22:22:09 -0800

    Finish default open graph image

--- src/lib/server/og-image.tsx
+++ src/lib/server/og-image.tsx
@@ -2,7 +2,9 @@ import type { FC } from 'hono/jsx';
 import * as meta from '$lib/meta';
 import ImageResponse from '@takumi-rs/image-response/wasm';
 import takumi from '@takumi-rs/wasm/takumi_wasm_bg.wasm?module';
-import notoSans from '@fontsource/noto-sans/files/noto-sans-latin-400-normal.woff2';
+import notoSans400 from '@fontsource/noto-sans/files/noto-sans-latin-400-normal.woff2';
+import notoSans800 from '@fontsource/noto-sans/files/noto-sans-latin-800-normal.woff2';
+import icon from '$lib/assets/favicon.svg?as=png&svg-as';
 
 interface Props {
 	title?: string;
@@ -16,16 +18,24 @@ const OpenGraphImage: FC<Props> = ({ title = meta.siteName, description = meta.d
 			height: '100%',
 			display: 'flex',
 			flexDirection: 'column',
+			justifyContent: 'center',
 			color: 'oklch(0.85 0.01 8)',
 			backgroundColor: 'oklch(13% 0.126 8deg)',
 			fontFamily: "'Noto Sans'",
 			fontWeight: 400,
 			fontStyle: 'normal',
-			padding: '60px'
+			padding: '3.5rem'
 		}}
 	>
-		<span style={{ fontSize: 32 }}>{title}</span>
-		<span>{description}</span>
+		<div style={{ display: 'flex', flexDirection: 'row', gap: '1rem', alignItems: 'center' }}>
+			<div style={{ width: '256px', height: '256px' }}>
+				<img src="icon" />
+			</div>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				<span style={{ fontSize: 80, fontWeight: 800 }}>{title}</span>
+				<span style={{ fontSize: 40 }}>{description}</span>
+			</div>
+		</div>
 	</div>
 );
 
@@ -42,8 +52,18 @@ const makeImage = ({ format = 'webp', debug = false, ...props }: Options = {}) =
 		height: 630,
 		devicePixelRatio: 1.0,
 		// This can be removed once using cloudflare vite plugin
-		// @ts-expect-error using a custom loader so woff2 is loaded as a Buffer, not string
-		fonts: [{ name: 'Noto Sans', data: notoSans, weight: 400, style: 'normal' }],
+		fonts: [
+			// @ts-expect-error using a custom loader so woff2 is loaded as a Buffer, not string
+			{ name: 'Noto Sans', data: notoSans400, weight: 400, style: 'normal' },
+			// @ts-expect-error using a custom loader so woff2 is loaded as a Buffer, not string
+			{ name: 'Noto Sans', data: notoSans800, weight: 800, style: 'normal' }
+		],
+		persistentImages: [
+			{
+				src: 'icon',
+				data: icon
+			}
+		],
 		drawDebugBorder: debug
 	});
 
