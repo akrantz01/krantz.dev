db2acf97d81d5bef8ff47a9370e799640c5269ea
Alex Krantz <alex@krantz.dev>
2026-01-09 12:49:34 -0800

    Pre-render default og image

--- src/routes/og/default/+server.ts
+++ src/routes/og/default/+server.ts
@@ -2,5 +2,7 @@ import type { RequestHandler } from './$types';
 import render from '$lib/server/og-image';
 import * as meta from '$lib/meta';
 
+export const prerender = true;
+
 export const GET: RequestHandler = ({ url }) =>
 	render(url, { type: 'default', title: meta.siteName, description: meta.description });
